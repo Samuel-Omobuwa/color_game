d8cd54c7be0b28602ccdbd5311b70e8603e370ae
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2025-02-06 22:55:11 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,40 @@ import './App.css';
 
 const colors = ['red', 'blue', 'yellow', 'green', 'purple'];
 
-const colorGame = () => {
+const ColorGame = () => {
 
 const [targetColor, setTargetColor] = useState('');
-const [ score, setScore] = useState(0)
-const[gameStatus, setGameStatus] = useState('');
+const [ score, setScore] = useState(0);
+const [gameStatus, setGameStatus] = useState('');
 
-useEffect(()=>{
+useEffect(() => {
 
   startNewGame();
-  
-}, [])
 
-const startNewGame = ()=> {
-  setTargetColor(getRandomColor(colors))
+}, []);
+
+const startNewGame = () => {
+  // setTargetColor(getRandomColor(colors));
+  setGameStatus('');
 }
 
+const handleColorGuess = (color)=> {
+  if (color === targetColor){
+    setScore(score + 1);
+    setGameStatus('Congratulations! 🎉')
+  } else {
+    setGameStatus('Oops! please try again!')
+  }
+};
 
+return (
+  <div>
 
+  </div>
+)
 }
+
+export default ColorGame;
 
 
 
